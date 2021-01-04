bee6d9b135ae20cf5f0571d827802d74e2881579
0yah <highmeme10@gmail.com>
2021-01-04 23:21:53 +0300

    Draw shapes and text

--- open_image.py
+++ open_image.py
@@ -2,8 +2,21 @@ import cv2
 from matplotlib import pyplot as plt
 import numpy as np
 
-#Load an image then convert it to greyscale
-img = cv2.imread("1.jpg",cv2.IMREAD_GRAYSCALE)
+img = cv2.imread("1.jpg",cv2.IMREAD_COLOR)
+
+
+#Draws A line
+cv2.line(img,(5,20),(150,150),(255,255,255),5)
+
+#Draws a rectangle
+cv2.rectangle(img,(15,25),(200,200),(20,20,20),10)
+
+#Draw a circle
+cv2.circle(img,(30,80),10,(40,40,40),1)
+
+#Draw Text
+font =cv2.FONT_HERSHEY_SIMPLEX # Select Font
+cv2.putText(img,"Text",(10,500),font,5,(50,50,50),5,cv2.LINE_AA)
 
 cv2.imshow("image",img)
 
