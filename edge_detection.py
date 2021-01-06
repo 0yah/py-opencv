3ba6def8fe9196a2200ed3bf85fc0a92c6a78d09
0yah <highmeme10@gmail.com>
2021-01-06 22:22:07 +0300

    Edge Detection v2

--- edge_detection.py
+++ edge_detection.py
@@ -13,17 +13,10 @@ while True:
 
     mask = cv2.inRange(hsv,lower_red,upper_red)
     res = cv2.bitwise_and(frame,frame,mask=mask)
-
-    laplacian = cv2.Laplacian(frame,cv2.CV_64F)
-    sobelx = cv2.Sobel(frame,cv2.CV_64F,1,0,ksize=5)
-    sobely = cv2.Sobel(frame,cv2.CV_64F,0,1,ksize=5)
-
     
     cv2.imshow('Original',frame)
-    cv2.imshow('Mask',mask)
-    cv2.imshow('laplacian',laplacian)
-    cv2.imshow('sobelx',sobelx)
-    cv2.imshow('sobely',sobely)
+    edges = cv2.Canny(frame,100,200)
+    cv2.imshow('Edges',edges)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
